3585667fbbe43bc21f0957069bc5d4737778a93b
A Little Rookie <983348993@qq.com>
2017-09-19 18:09:54 +0800

    +

--- js_高级程序设计学习笔记(5~8章).js
+++ js_高级程序设计学习笔记(5~8章).js
@@ -141,6 +141,142 @@
 				}
 
 		8.操作方法
+			concat() 创建当前数组一个副本，在接收参数的条件下添加到副本的末尾，若没有参数，则返回副本
+				var colors = ["red","yellow","blue"];
+				var colors2 = colors.concat("green",["black","brown"]);
+				alert(colors2);//red,yellow,blue,green,black,brown
+
+			slice() 一个参数情况下，返回从参数位置开始到数组末尾所有项。
+					两个参数，返回两个参数位置之间的项，但不包括结束项。
+					此方法不会影响原始数组
+
+				var colors = ["red","green","blue","yellow","purple"];
+				var colors2 = colors.slice(1);
+				var colors3 = colors.slice(1,4);
+				alert(colors2);//green,blue,yellow,purple
+				alert(colors3);//green,blue,yellow
+
+			splice(); 删除 插入 替换  此方法会替换掉原始数组  返回的项是删除的项的数组
+
+				var colors = ["red","green","blue"]; 
+				var removed = colors.splice(0,1); //（删除操作）0 删除的起始位置  1 删除项的个数  删除第一项
+				alert(colors);//green,blue
+				alert(removed);// red
+
+				removed = colors.splice(1,0,"yellow","orange");// （插入操作） 1 插入的位置  0  删除0个项  后面两个参数是插入的值
+				alert(colors);//green,yellow,orange,blue
+				alert(removed);// 返回空数组
+
+				removed = colors.splice(1,1,"red","purple");// （替换操作） 1 插入的位置  1  删除1个项  后面两个参数是插入的值
+				alert(colors);//green,red,purple,orange,blue
+				alert(removed);//yellow
+
+		9.位置方法  兼容性IE9+
+			indexOf();接收两个参数   要查找的项  和查找起点位置的索引（可选参数）从前向后 找不到返回-1
+			lastIndexOf(); 从后向前查找
+
+				var numbers = [1,2,3,4,5,4,3,2,1];
+				alert(numbers.indexOf(4));//3  返回该项的索引值
+				alert(numbers.lastIndexOf(4));//5  返回第一个该项的索引值
+
+		10.迭代方法（简单举例，具体google） 兼容性IE9+
+			every():
+				var numbers = [1,2,3,4,5,4,3,2,1];
+				var everyResult = numbres.every(function(item,index,array){
+					return(item > 2);
+				})
+				alert(everyResult);//false   必须每一项都符合
+
+			some():
+				var numbers = [1,2,3,4,5,4,3,2,1];
+				var someResult = numbres.some(function(item,index,array){
+					return(item > 2);
+				})
+				alert(someResult);//true   有一项符合就是true
+
+			filter():
+				var numbers = [1,2,3,4,5,4,3,2,1];
+				var filterResult = numbres.filter(function(item,index,array){
+					return(item > 2);
+				})
+				alert(filterResult);//[3,4,5,4,3]   返回符合的项
+
+			map():
+				var numbers = [1,2,3,4,5,4,3,2,1];
+				var mapResult = numbres.map(function(item,index,array){
+					return item * 2 ;
+				})
+				alert(mapResult);//[2,4,6,8,10,8,6,4,2]   
+
+			forEach();//本质上跟for循环一样
+				var numbers = [1,2,3,4,5,4,3,2,1];
+				numbres.forEach(function(item,index,array){
+					//执行某些操作
+				})
+
+		11.归并方法 兼容性IE9+
+			reduce()://数组求和  四个参数分别是第一个值 当前值 索引 数组  返回结果迭代传给第一个值
+				var values = [1,2,3,4,5];
+				var sum = values.reduce(function(prev,cur,index,array){
+					return prev + cur;
+				})
+				alert(sum);//15
+
+			reduceRight():从右边开始迭代  顺序倒一下
+
+	三.Date类型
+		1.从1970.1.1号开始计算
+			创建日期对象：
+				var now = new Date();//自动获得当前时间
+
+				var now = new Date("May 25, 2014");//后台自动调用了下面代码
+				var now = new Date(Date.parse("May 25, 2014"));//两组代码等同  格式不限  格式错了会报错NaN
+
+				2015年5月5号下午5：5：55 （月份0~11 小时0~23 天数1~31）
+				var now = new Date(2015,4,5,17,55,55);//后台自动调用了下面代码
+				var now = new Date(Date.UTC(2015,4,5,17,55,55));//两组代码等同  
+
+				2000年1月1日午夜零时
+				var now = new Date(2000,0);//后台自动调用了下面代码
+				var now = new Date(Date.UTC(2000,0));//两组代码等同 
+
+		2.时间戳
+			Date.now();//调用这个方法的日期和时间的毫秒数  IE9+兼容  等同于+new Date()
+				var start = Date.now();
+				doSomething();
+				var stop = Date.now();
+				var result = stop - start;
+
+		3.日期格式化方法
+			toDateString();日期格式转化为字符串
+			toTimeString();
+			toLocalDateString();
+			toLocalTimeString();
+
+		4.日期/时间组件方法
+			太多  具体google
+
+	四.RegExp类型 正则表达式  （太多  不详写了）
+		var expression = /pattern/flags;
+		var expression = new RegExp("pattern","flags");
+
+			var pattern1 = /[bc]at/i;//匹配第一个bat或cat,不区分大小写
+			var pattern1 = new RegExp("[bc]at","i");//匹配第一个bat或cat,不区分大小写
+
+		flags标志:
+			g:全局模式
+			i:不区分大小写模式
+			m:多行模式
+
+	五.Function类型
+
+
+
+
+
+
+
+
 				
 
 
